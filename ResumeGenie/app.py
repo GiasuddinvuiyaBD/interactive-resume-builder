1506532097abe2163fde3d4da17cd71dd5adbba9
Gias uddin vuiya <giasuddinvuiya2022@gmail.com>
2024-12-26 12:09:47 +0600

    added two new files and modifyed app.py

--- ResumeGenie/app.py
+++ ResumeGenie/app.py
@@ -11,6 +11,17 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
+# Ensuser that users get up to date data
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 @app.route("/")
 def home():
 
